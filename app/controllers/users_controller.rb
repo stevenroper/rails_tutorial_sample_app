03cd232c7a083d419f9f61e9ebe6a5af47988d15
Steven Roper <sroper@masteryconnect.com>
2015-12-20 11:25:46 -0700

    Create basic form signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
-  def new
-  end
-
+  
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)     #Replace with final version. Kind of a hacky start
+    if @user.save
+      # Success message
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
