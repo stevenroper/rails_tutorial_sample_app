e6f9565102ea33efe7c2ac6b4ce1e59a6e2fa8d7
Steven Roper <sroper@masteryconnect.com>
2016-01-18 17:40:41 -0700

    Finish exercise 1

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -72,4 +72,21 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert_redirected_to user
   end
 
+  test 'expired password reset' do
+    get new_password_reset_path
+    post password_resets_path, password_reset: { email: @user.email }
+
+    @user = assigns(:user)
+    @user.update_attribute(:reset_sent_at, 3.hours.ago)
+    patch password_reset_path(@user.reset_token),
+      email: @user.email,
+      user: { password: 'foobar',
+              password_confirmation: 'foobar'
+            }
+    assert_response :redirect
+    follow_redirect!        
+    assert_not flash.empty?
+    assert_match 'reset has expired', response.body
+  end
+
 end
