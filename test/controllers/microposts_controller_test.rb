50e7440ff5b4f27c4cbf962bc82359c5f1ba3a29
Steven Roper <sroper@masteryconnect.com>
2016-02-06 17:03:22 -0700

    Add micropost form and feed.

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @micropost = microposts(:architecture)
+  end
+
+  test 'should redirect create if not logged in' do
+    assert_no_difference 'Micropost.count' do
+      post :create, micropost: { content: 'Things' }
+    end
+    assert_redirected_to login_url
+  end
+
+  test 'shoud redirect delete if not logged in' do
+    assert_no_difference 'Micropost.count' do
+      post :destroy, id: @micropost
+    end
+    assert_redirected_to login_url
+  end
 end
